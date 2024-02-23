71a3316b6eb1b41edd4c6edd8a3e65d3865934bf
Nardo <bm.hcmc.nardo@modetour.com>
2024-02-23 13:47:44 +0700

    check code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import feather from 'feather-icons';
+import AppHeader from './components/shared/AppHeader';
+import AppFooter from './components/shared/AppFooter';
+
+export default {
+	components: {
+		AppHeader,
+		AppFooter,
+	},
+	data: () => {
+		return {
+			appTheme: localStorage.getItem('theme'),
+		};
+	},
+	mounted() {
+		feather.replace();
+	},
+	updated() {
+		feather.replace();
+	},
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue??" />
+	<div :class="appTheme" class="pt-0.5">
+		<!-- App header -->
+		<AppHeader />
+
+		<!-- Render active component contents with vue transition -->
+		<transition name="fade" mode="out-in">
+			<router-view :theme="appTheme" />
+		</transition>
+
+		<!-- Scroll to top -->
+		<back-to-top
+			visibleoffset="500"
+			right="30px"
+			bottom="20px"
+			class="shadow-lg"
+		>
+			<i data-feather="chevron-up"></i>
+		</back-to-top>
+
+		<!-- App footer -->
+		<AppFooter />
+	</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.vue-back-to-top {
+	@apply p-2 bg-indigo-500 hover:bg-indigo-600 text-white transition
+        duration-500
+        ease-in-out
+        transform
+        hover:-translate-y-1 hover:scale-110;
+	border-radius: 50%;
+	font-size: 22px;
+	line-height: 22px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.fade-enter-active {
+	animation: coming 0.4s;
+	animation-delay: 0.2s;
+	opacity: 0;
+}
+
+.fade-leave-active {
+	animation: going 0.4s;
+}
+
+@keyframes going {
+	from {
+		transform: translateX(0);
+	}
+
+	to {
+		transform: translateX(-10px);
+		opacity: 0;
+	}
+}
+
+@keyframes coming {
+	from {
+		transform: translateX(-10px);
+		opacity: 0;
+	}
+
+	to {
+		transform: translateX(0px);
+		opacity: 1;
+	}
 }
 </style>
