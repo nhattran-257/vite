1b285af165ec30afe5574bef9d25477f252dee50
Nardo <bm.hcmc.nardo@modetour.com>
2024-12-03 14:05:45 +0700

    d

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,26 @@
 import AppBanner from "@/components/shared/AppBanner";
 import ProjectsGrid from "../components/projects/ProjectsGrid.vue";
 import Button from "../components/reusable/Button.vue";
+
+export default {
+  name: "Home",
+  components: {
+    AppBanner,
+    ProjectsGrid,
+    Button,
+  },
+};
 </script>
 
 <template>
-  <div class="container mx-auto"></div>
+  <div class="container mx-auto">
+    <!-- Banner -->
+    <AppBanner class="mb-5 sm:mb-8" />
+
+    <!-- Projects -->
+
+    <!-- Load more projects button -->
+  </div>
 </template>
 
 <style scoped></style>
